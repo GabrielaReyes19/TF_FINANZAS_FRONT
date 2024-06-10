bba8bfceaf9924d8320b8434b3ed4022d4da4cd8
Gabriela Reyes <107740335+GabrielaReyes19@users.noreply.github.com>
2024-06-10 01:26:13 -0500

    Register new
    falta imagen

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { DashboardComponent } from './pages/dashboard/dashboard.component';
 import { LoginComponent } from './auth/login/login.component';
+import { RegisterComponent } from './auth/register/register.component';
 
 const routes: Routes = [
   {path:'',redirectTo:'/inicio', pathMatch:'full'},
-  {path:'inicio',component:LoginComponent}
+  {path:'inicio',component:LoginComponent},
+  { path: 'register', component: RegisterComponent },
 ];
 
 @NgModule({
